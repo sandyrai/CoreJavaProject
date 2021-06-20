ad1c854dfc3f2b2007a10a96a49409fc336471a4
sandyrai <kmr.sandeep1997@gmail.com>
2021-06-20 21:05:30 +0530

    variable completed

--- CoreJavaProject/src/core/java/basic/variable/Variable.java
+++ CoreJavaProject/src/core/java/basic/variable/Variable.java
@@ -2,4 +2,13 @@ package core.java.basic.variable;
 
 public class Variable {
 
+	
+	int x=10; //integer
+	String str="test"; //string variable 
+	double y=2000000000; //
+	boolean falg=false; //boolean variable
+	
+	string str2=100000; //invalid value in string --> use ' or  "
+	
+	
 }
